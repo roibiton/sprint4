90d6ae820d22c1395ab180c5a4d725fe02644a27
Denis <denismagin@gmail.com>
2022-09-15 15:48:24 +0300

    name and css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
+import './assets/styles/main.scss'
 import { store } from './store/store'
 import { RootCmp } from './root-cmp'
 
