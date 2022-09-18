480ea181e1c9cd12c8fc3f129524224ca32dd6ff
AmitosGitHub <amit.osman@gmail.com>
2022-09-18 21:56:55 +0300

    change filter header

---
+++ frontend/src/cmps/display-main-search.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState, useRef } from 'react'
+
+import { BiSearch } from 'react-icons/bi'
+
+export const DisplayMainSearch = () => {
+  const [type, setType] = useState({
+    stays: true,
+    experiences: false,
+    onlineEx: false,
+  })
+  return (
+    <section className="display-main-search">
+      <div className="main-header-selected">
+        <button>Stays</button>
+        <button>Experiences</button>
+        <button>Online Experiences</button>
+      </div>
+      <div className="second-header-selected">
+        <button className="btn btn-where">
+          <h2>Where</h2>
+          <h3>Search destinations</h3>
+        </button>
+        <button className="btn btn-check-in">
+          <h2>Check in</h2>
+          <h3>Add dates</h3>
+        </button>
+        <button className="btn btn-check-out">
+          <h2>Check out</h2>
+          <h3>Add dates</h3>
+        </button>
+        <div className="btn-design">
+          <button className="btn-who">
+            <h2>Who</h2>
+            <h3>Add guests</h3>
+          </button>
+          <button className="btn-search">
+            <BiSearch className=" search-icon" />
+            <span> Search</span>
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
