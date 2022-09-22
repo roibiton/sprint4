f57c85681bba404c928207aca2d385ce802e8c33
roibiton <roibitonroi@gmail.com>
2022-09-22 14:40:07 +0300

    change editId

--- frontend/src/cmps/stay-preview.jsx
+++ frontend/src/cmps/stay-preview.jsx
@@ -28,11 +28,6 @@ export function StayPreview({ stay, onRemoveStay, onUpdateStay }) {
     // console.log('stay.owner',loggedinUser)
     return (
         <div className='stay-preview'>
-            {/* <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" class="liked"
-                style="display: block; fill: rgba(0, 0, 0, 0.5); height: 24px; width: 24px; stroke: var(--f-mkcy-f); stroke-width: 2; overflow: visible;">
-                <path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.05
-                1-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path>
-            </svg> */}
             <div className='preview-img-container'>
                 <ImgCarousel imgUrls={stay.imgUrls} />
                 {/* <img className='preview-img' src='https://a0.muscache.com/im/pictures/47d23608-568e-4a1d-b2c0-5cd74a20bc22.jpg?im_w=1200' alt="" /> */}
@@ -43,7 +38,7 @@ export function StayPreview({ stay, onRemoveStay, onUpdateStay }) {
             <Link to={`/stay/${stay._id}`} className='info'>
 
                 <div className='preview-text-container'>
-                    {/* <div><i className="fa-solid fa-gamepad"></i></div> */}
+                    
                     <section className='preview-title-container'>
                         <div className='preview-title'><span>{stay.loc.city}</span>,<span>{stay.loc.country}</span></div>
                         <div>
@@ -51,9 +46,7 @@ export function StayPreview({ stay, onRemoveStay, onUpdateStay }) {
                             <span>{stay.rate}</span>
                         </div>
                     </section>
-                    {/* <p><small>{stay.type}</small>-|-
-                        <small>Owner: <span>{stay.owner && stay.owner.fullname}</span></small>
-                    </p> */}
+                    
                     <section className='preview-details-container'>
                         <p><small>{stay.type}</small></p>
                         <small><span>{(destination/1000).toLocaleString()}</span><span>kilometers</span></small>
@@ -66,6 +59,7 @@ export function StayPreview({ stay, onRemoveStay, onUpdateStay }) {
             <section className='actions'>
                 {/* <button onClick={() => { onRemoveStay(stay._id) }}>❌</button> */}
                 {(stay?.owner === loggedinUser) && <Link to={`/stay/edit/${stay._id}`} ><button>🖍</button></Link>}
+                { <Link to={`/stay/edit/${stay._id}`} ><button>🖍</button></Link>}
             </section>
 
         </div>
